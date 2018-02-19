c26dc27bab3a33524c083e153c2b3b85c56a6015
JTH28 <jhudspeth24@gmail.com>
2018-02-19 17:58:32 -0600

    Added some arrow functions

--- src/App.js
+++ src/App.js
@@ -2,15 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org',
+        author: 'Jordan Walke',
+        num_comments: 2,
+        points: 4,
+        objectId: 0,
+    },
+
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectId: 1,
+    }
+]
+
 class App extends Component {
   render(){
     const text = "Welcome to the Road to learn React";
     const user = { first : 'Jason', last : 'H'}
     return (
       <div className="App">
-          <h2>{text}, {user.first} {user.last}  </h2>
-      </div>
-    );
+          {list.map(item =>
+              <div key={item.objectId}>
+                  <span>
+                      <a href={item.url}>{item.title}</a>
+                  </span>
+                  <span>{item.author}</span>
+                  <span>{item.num_comments}</span>
+                  <span>{item.points}</span>
+              </div>)}
+      </div>);
   }
 }
 
