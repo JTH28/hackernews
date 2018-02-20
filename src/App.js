91de775ea3d3b8987d157dae36e520f9f54d7cf9
JTH28 <jhudspeth24@gmail.com>
2018-02-20 16:09:07 -0600

    Added constructor

--- src/App.js
+++ src/App.js
@@ -2,36 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const list = [
-    {
-        title: 'React',
-        url: 'https://reactjs.org',
-        author: 'Jordan Walke',
-        num_comments: 2,
-        points: 4,
-        objectId: 0,
-    },
-
-    {
-        title: 'Redux',
-        url: 'https://redux.js.org',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectId: 1,
+class Stuff {
+    constructor(title,url,author, num_comments,points, objectId){
+        this.title = title;
+        this.author =author;
+        this.num_comments = num_comments;
+        this.points = points;
+        this.objectId = objectId;
     }
-]
+}
+
+const list = []
+
+list.push(new Stuff('React',
+        'https://reactjs.org',
+        'Jordan Walke',
+        2, 4, 0))
+
+list.push(new Stuff('Redux',
+        'https://redux.js.org',
+        'Dan Abramov, Andrew Clark',
+        2,5,1))
 
 class App extends Component {
+  construstor(props){
+    super(props);
+
+    this.state = {
+        list: list
+    }
+  }
+
   render(){
     const text = "Welcome to the Road to learn React";
     const user = { first : 'Jason', last : 'H'}
     return (
       <div className="App">
-          {list.map(item =>
+          {list.map((item) =>
               <div key={item.objectId}>
                   <span>
-                      <a href={item.url}>{item.title}</a>
+                      <a href={item.url}>{item.title} </a>
                   </span>
                   <span>{item.author}</span>
                   <span>{item.num_comments}</span>
